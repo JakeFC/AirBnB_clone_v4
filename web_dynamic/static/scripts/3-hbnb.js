12b19b9895e849fd73d8a1ce512400ee71a6253c
JakeFC <jacobchavera@yahoo.com>
2021-06-29 23:11:18 +0000

    task 4 js file

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -22,3 +22,37 @@ $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
     $('#api_status').removeClass('available');
   }
 });
+
+$.ajax({
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  data: JSON.stringify({}),
+  method: 'POST',
+  dataType: 'json',
+  processData: false,
+  contentType: 'application/json',
+  success: function (places) {
+    for (const n in places) {
+      const place = places[n];
+      let article = `<article>\
+        <div class="title_box">\
+        <h2>${place.name}</h2>\
+        <div class="price_by_night">$${place.price_by_night}</div>\
+        </div>\
+        <div class="information">\
+        <div class="max_guest">${place.max_guest} Guest`;
+      if (place.max_guest !== 1) {
+        article += 's';
+      }
+      article += `</div><div class="number_rooms">${place.number_rooms} Bedroom`;
+      if (place.number_rooms !== 1) {
+        article += 's';
+      }
+      article += `</div><div class="number_bathrooms">${place.number_bathrooms} Bathroom`;
+      if (place.number_bathrooms !== 1) {
+        article += 's';
+      }
+      article += `</div></div><div class="description">${place.description}</div></article>`;
+      $('.places').append(article);
+    }
+  }
+});
