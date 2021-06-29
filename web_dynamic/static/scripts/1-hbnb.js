8364417390f79b043f66726d89c46d936a9f663f
JakeFC <jacobchavera@yahoo.com>
2021-06-29 00:49:51 +0000

    fixed syntax and file reference errors

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,16 @@
 // updates text under aDict with all checked amenity names
-aDict = {};
+const aDict = {};
 $('input').click(function () {
   $(':input').each(function () {
-    if (this.checked == true) {
+    if (this.checked === true) {
       aDict[$(this).data('name')] = $(this).data('id');
     } else {
       delete aDict[$(this).data('name')];
     }
   });
-  arr = [];
+  const arr = [];
   for (const key in aDict) {
-    if (aDict.hasOwnProperty(key)) {
-      arr.push(key);
-    }
+    arr.push(key);
   }
   $('.amenities h4').html(arr.sort().join(', '));
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,28 +1,24 @@
 // updates text under aDict with all checked amenity names
-aDict = {};
+const aDict = {};
 $('input').click(function () {
   $(':input').each(function () {
-    if (this.checked == true) {
+    if (this.checked === true) {
       aDict[$(this).data('name')] = $(this).data('id');
     } else {
       delete aDict[$(this).data('name')];
     }
   });
-  arr = [];
+  const arr = [];
   for (const key in aDict) {
-    if (aDict.hasOwnProperty(key)) {
-      arr.push(key);
-    }
+    arr.push(key);
   }
   $('.amenities h4').html(arr.sort().join(', '));
 });
 
-$.getJSON("http://0.0.0.0:5001/api/v1/status/", function(data) {
-    if (data.status === 'OK') {
-        $('#api_status').addClass('avalible'); 
-    } else {
-        $('#api_status').removeClass('avalible');
-      }
-
-
+$.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
 });
